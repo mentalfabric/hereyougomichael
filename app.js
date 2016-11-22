c9c275ee70128abdd7b1d49395add816389589de
pratimasakinala <pratimasakinala13@gmail.com>
2016-11-21 20:26:27 -0800

    table routing DONE!!!!

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var tables = require('./routes/tables');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/tables', tables);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
